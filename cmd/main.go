972f28ef27ec03e287fb29435a1096aec9c16802
Jay <Jay@binance.com>
2021-05-13 23:58:49 +0800

    endless for 优雅重启

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/brightoneqq/go-pegasus/pkg/router"
 	"github.com/brightoneqq/go-pegasus/pkg/setting"
-	"net/http"
+	"github.com/fvbock/endless"
+	"log"
+	"syscall"
 )
 
 func main() {
@@ -14,15 +16,28 @@ func main() {
 	//		"message": "test",
 	//	})
 	//})
-	x := router.InitRouter()
-
-	s := &http.Server{
-		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler:        x,
-		ReadTimeout:    setting.ReadTimeout,
-		WriteTimeout:   setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
+	log.Println("endpoint " + endPoint)
+	server := endless.NewServer(endPoint, router.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("Actual pid is %d", syscall.Getpid())
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
 	}
 
-	s.ListenAndServe()
+	//x := router.InitRouter()
+	//
+	//s := &http.Server{
+	//	Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
+	//	Handler:        x,
+	//	ReadTimeout:    setting.ReadTimeout,
+	//	WriteTimeout:   setting.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//s.ListenAndServe()
 }
